fd995a82e2bb26dd27b8c35eb3ac1c210f16047d
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2026-03-04 17:28:04 +0300

    update css styles

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,22 @@ import _ from 'lodash';
 import '../scss/index.scss';
 
 
+/* Скрыть всплывающий блок уведомления на главной странице: */
+/* вариант 1: */
+// document.getElementById('closeButton').addEventListener('click', function() {
+//   document.getElementById('myElement').style.display = 'none';
+// });
+/* вариант 2: */
+// document.getElementById('closeButton').addEventListener('click', function() {
+//   document.getElementById('myElement').classList.add('hidden');
+// });
+
+/* вариант 3 (более плавное исчезновение блока): */
+document.getElementById('closeButton').addEventListener('click', function() {
+  const element = document.getElementById('myElement');
+  element.classList.add('fade-out');
+  // Удаляем элемент из потока после завершения анимации
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, 500);
+});
