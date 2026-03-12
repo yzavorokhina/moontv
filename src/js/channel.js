a96a78021e006a1353f356a12a9aa1f6807c8899
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2026-03-13 00:29:15 +0300

    add chat clips and video for channel page

--- src/js/channel.js
+++ src/js/channel.js
@@ -2,6 +2,7 @@ import '../scss/vendors/_bootstrap.scss';
 import '../scss/vendors/_normalize.scss';
 // import '../scss/index.scss';
 import '../scss/channel.scss';
+import './components/chat';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Channel page loaded with its styles');
