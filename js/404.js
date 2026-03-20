28e238245c1711913c3094d4e1e1ab90c922d401
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2026-03-20 14:56:20 +0300

    Updates

---
+++ js/404.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",()=>{console.log("404 page loaded with its styles")})})();
+//# sourceMappingURL=404.js.map
---
+++ js/auth.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",()=>{console.log("Auth page loaded with its styles")})})();
+//# sourceMappingURL=auth.js.map
