2ce7c77b3f9c04ca541b5ba824a7d088f97c5b15
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2026-03-21 00:07:17 +0300

    add some fix for auth and 404 pages

--- src/js/404.js
+++ src/js/404.js
@@ -6,5 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('404 page loaded with its styles');
 });
 
+/* MY TODO: make render for squares with map like in games: */
+// const boxMap = "000X000"
 
-const boxMap = "000X000"
+/* Redirect to main page after some interval, variant1: */
+// Start the timer: every 5 seconds, check if it's time to make a redirect:
+// const redirectTimer = setInterval(() => {
+//   // After 5 seconds, stop the timer and perform the redirect:
+//   clearInterval(redirectTimer);
+//   window.location.href = './index.html';
+// }, 5000);
+
+/* Redirect to main page after some interval, variant2: */
+// setTimeout(() => {
+//   window.location.href = './index.html';
+// }, 5000); // redirect after 5 seconds (this is the maximum for user's comfort)
